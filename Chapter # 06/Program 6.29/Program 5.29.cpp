3c695963c78be5ea033f1e7c796de659ed9ac878
Cyber Hawk <cs.cybernetic@gmail.com>
2021-03-20 08:16:34 +0500

    Tasleem Mustafa Complete Book Program's Solutions

---
+++ Chapter # 06/Program 6.29/Program 5.29.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int c,num=1;
+	cout<<"The Series is as follow:\n";
+	for(c=1;num<=40;c++)
+	{
+		cout<<num<<"   ";
+		num=num+3;
+	}
+	return 0;
+}
